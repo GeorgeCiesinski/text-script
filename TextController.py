cdbbefaae562e3b9217fb4ac50945f2b4ca11363
George Ciesinski <george.ciesin@gmail.com>
2020-01-03 15:02:38 -0500

    Basic working concept is complete

--- TextController.py
+++ TextController.py
@@ -1,14 +1,17 @@
-from pynput.keyboard import Listener
+from pynput.keyboard import Controller, Key, Listener
 from Logger import Logger
 
 
 # Class catches individual words as they are typed
 class WordCatcher:
 
-    def __init__(self, log):
+    def __init__(self, log, keyboard):
 
         # Creates instance wide log variable
         self.log = log.log
+
+        # Creates instance wide typer variable
+        self.keyboard = keyboard
 
         # Temporary word variable
         self.word_in_progress = False
@@ -38,7 +41,6 @@ class WordCatcher:
         - delete word after it is printed
 
         :param key:
-        :return:
         """
 
         # Sets keypress to instance key value
@@ -48,20 +50,20 @@ class WordCatcher:
         self.keycode_to_keydata()
 
         # Checks delimiter
-        self.delimiter_check()
+        self.check_delimiter()
 
         # Checks if word has ended
-        self.word_end_check()
+        self.check_word_end()
 
         # Checks for backspace
-        self.backspace_check()
+        self.check_backspace()
 
         # Appends letter if word is in progress
-        self.letter_append()
+        self.append_letter()
 
     def keycode_to_keydata(self):
         """
-        Converts KeyCode to raw key value.
+        Converts KeyCode to string and strips quotations (into variable: keydata).
         """
 
         # Converts KeyData to string, strips ' from result
@@ -71,9 +73,9 @@ class WordCatcher:
         # Prints typed letter to console
         print(self.keydata)
 
-    def delimiter_check(self):
+    def check_delimiter(self):
         """
-        Checks if delimiter has been entered
+        Checks if delimiter has been entered. Either starts self.current_word or restarts it.
         """
 
         # If delimiter is entered but there is a word in progress, clear the word and start a new word
@@ -93,7 +95,7 @@ class WordCatcher:
 
             self.log.debug("Delimiter detected. Starting new word.")
 
-    def word_end_check(self):
+    def check_word_end(self):
         """
         Checks if Key.tab, Key.space, or Key.enter is pressed. Prints word if pressed.
         """
@@ -103,12 +105,17 @@ class WordCatcher:
             # Checks if there is a word in progress, clears it if true
             if self.word_in_progress is True:
 
+                self.check_shortcut()
+
                 self.log.debug(f"Word ended by {self.keydata}: {self.current_word}")
 
                 # Clears current word
                 self.clear_current_word()
 
-    def backspace_check(self):
+    def check_backspace(self):
+        """
+        Checks if backspace was pressed. Erases last letter if pressed.
+        """
 
         if self.keydata == "Key.backspace":
 
@@ -117,7 +124,7 @@ class WordCatcher:
 
             self.log.debug("Key.backspace entered, removing last letter from word.")
 
-    def letter_append(self):
+    def append_letter(self):
         """
         Appends the letter to self.current_word if self.word_in_progress is true
         """
@@ -129,7 +136,20 @@ class WordCatcher:
 
             self.log.debug(f"Appended {self.keydata} to self.current_word.")
 
+    def check_shortcut(self):
+        """
+        Checks list of shortcuts for a match. Sets text block if match is found.
+        """
+
+        if self.current_word == "#fuck":
+
+            self.keyboard.delete_shortcut(self.current_word)
+            self.keyboard.type_block()
+
     def clear_current_word(self):
+        """
+        Replaces self.current_word with empty string, and sets self.word_in_progress to False
+        """
 
         self.current_word = ""
         self.word_in_progress = False
@@ -137,8 +157,26 @@ class WordCatcher:
         self.log.debug("self.current_word changed to False.")
 
 
-class TextTyper:
-    pass
+class KeyboardEmulator:
+
+    def __init__(self, log):
+
+        # Initializes controller
+        self.c = Controller()
+
+    def delete_shortcut(self, current_word):
+        """
+        Deletes the shortcut that the user typed in.
+        """
+
+        word_length = len(current_word)
+        for i in range(word_length + 1):
+            self.c.press(Key.backspace)
+            self.c.release(Key.backspace)
+
+    def type_block(self):
+
+        self.c.type("Eat a bag of dicks motherfucker, and FUCK you!")
 
 
 if __name__ == "__main__":
@@ -147,5 +185,8 @@ if __name__ == "__main__":
     L = Logger()
     L.log.debug("Program started from TextController.py. Debugging.")
 
-    # Creates instance of WordCatcher
-    w = WordCatcher(L)
+    # Initializes KeyboardEmulator instance
+    k = KeyboardEmulator(L)
+
+    # Initializes WordCatcher instance
+    w = WordCatcher(L, k)
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from Logger import Logger
-from TextController import WordCatcher
+from TextController import WordCatcher, KeyboardEmulator
 
 # Initialize Logger
 L = Logger()
 L.log.debug("Program started from App.py.")
 
+# Initializes KeyboardEmulator instance
+k = KeyboardEmulator(L)
+
 # Initialize WordCatcher
-w = WordCatcher(L)
+w = WordCatcher(L, k)
