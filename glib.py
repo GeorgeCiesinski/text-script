8e764f6ae9b1c1fd5d4ea6fd92a8e082246b1b54
George Ciesinski <george.ciesin@gmail.com>
2020-01-05 22:05:46 -0500

    Added two new methods: list subdirectories, and list files

--- glib.py
+++ glib.py
@@ -2,8 +2,15 @@ import os
 
 
 def list_subdirectories(directory):
+    """
+    Lists all subdirectories.
+
+    :param directory:
+    :return:
+    """
 
     directory_list = list()
+
     for root, dirs, files in os.walk(directory, topdown=False):
         for name in dirs:
             directory_list.append(os.path.join(root, name))
@@ -11,5 +18,24 @@ def list_subdirectories(directory):
     print(directory_list)
 
 
+def list_files(directory):
+    """
+    Lists all files and subdirectories in the directory.
+
+    :param directory:
+    :return:
+    """
+
+    file_list = list()
+
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            file_list.append(os.path.join(root, name))
+
+    print(file_list)
+
+
 text_block_dir = 'Textblocks/'
 list_subdirectories(text_block_dir)
+
+list_files(text_block_dir)
