f829fa60e9f0dbefc55ba26540847b1aa00f7d26
George Ciesinski <gciesinski@corp.pinnaca.com>
2020-01-10 14:08:50 -0500

    Modified app.py by splitting work into functions to be called by threading

--- app.py
+++ app.py
@@ -1,33 +1,49 @@
 import glib
+import threading
 from Logger import Logger
 from Gui import Gui
 from TextController import WordCatcher, KeyboardEmulator
+
+
+def start_text_controller(logger):
+
+    # Gets file_list and file_dir_list
+    textblock_dir = "Textblocks/"
+
+    file_list, file_dir_list = glib.list_files(textblock_dir)
+
+    # Creates shortcut list with the same index
+    shortcut_list = glib.list_shortcuts(file_list)
+
+    # Initializes KeyboardEmulator instance
+    k = KeyboardEmulator(logger)
+
+    # Initialize WordCatcher
+    w = WordCatcher(logger, k, shortcut_list, file_dir_list)
+
+
+def start_gui(logger):
+
+    # Main instance logger
+    log = logger.log
+
+    # Create GUI object
+    g = Gui(logger)
+
+    # Close program if window is destroyed
+    g.root.protocol("WM_DELETE_WINDOW", g.on_closing)
+
+    # Tkinter main loop
+    log.debug("Starting Tkinter mainloop.")
+    g.root.mainloop()
+
 
 # Initialize Logger
 L = Logger()
 
 L.log.debug("Program started from App.py.")
 
-# Gets file_list and file_dir_list
-textblock_dir = "Textblocks/"
+# Initializes process to start
+start_text_controller(L)
 
-file_list, file_dir_list = glib.list_files(textblock_dir)
-
-# Creates shortcut list with the same index
-shortcut_list = glib.list_shortcuts(file_list)
-
-# Create GUI object
-g = Gui(L)
-
-# Initializes KeyboardEmulator instance
-k = KeyboardEmulator(L)
-
-# Initialize WordCatcher
-w = WordCatcher(L, k, shortcut_list, file_dir_list)
-
-# Close program if window is destroyed
-g.root.protocol("WM_DELETE_WINDOW", g.on_closing)
-
-# Tkinter main loop
-L.log.debug("Starting Tkinter mainloop.")
-g.root.mainloop()
+start_gui(L)
