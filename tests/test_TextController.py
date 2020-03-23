06135d4b3071add7108f7c52e8435157d34b4562
George Ciesinski <gciesinski@corp.pinnaca.com>
2020-03-23 12:12:02 -0400

    Changed build environment to windows

--- tests/test_TextController.py
+++ tests/test_TextController.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.abspath('textscript'))
 
-from TextController import WordCatcher
+from TextController import WordCatcher, KeyboardEmulator
 
 def test_basic():
     pass
