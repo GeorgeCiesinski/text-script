42438e58a0b76e7c28d7d0df4b9c3e164ec27c43
George Ciesinski <gciesinski@corp.pinnaca.com>
2020-03-23 19:44:02 -0400

    Fixed build failure temporarily.

--- tests/test_TextController.py
+++ tests/test_TextController.py
@@ -2,7 +2,13 @@ import os
 import sys
 sys.path.append(os.path.abspath('textscript'))
 
-from TextController import WordCatcher, KeyboardEmulator
+# from TextController import WordCatcher, KeyboardEmulator
+
+"""
+This testing is pending resolution of issue #59 on the repository. The above import is disabled to prevent the build
+from failing. At this time, if you import WordCatcher or KeyboardEmulator, the pynput library tries to use X, which 
+produces an error on Travis CI. 
+"""
 
 def test_basic():
     pass
